1e16a75d31f2a5838ffe96de3285c1ab84dc3a92
dangphu2412 <dangphu241299@gmail.com>
2022-10-27 22:37:18 +0700

    feat: refactor error handling

---
+++ src/authentication/client/exceptions/index.ts
@@ -0,0 +1,1 @@
+export * from './incorrect-username-password.exception';
--- src/exception/exception-client-code.constant.ts
+++ src/exception/exception-client-code.constant.ts
@@ -1,25 +1,4 @@
-import {
-  generateClientException,
-  generateSystemException,
-} from './exception-generator';
-
-export const AuthExceptionClientCode = {
-  INCORRECT_USERNAME_OR_PASSWORD: generateClientException({
-    errorCode: 'AUTH__INCORRECT_USERNAME_OR_PASSWORD',
-    message: 'Incorrect username or password',
-  }),
-  LOGOUT_REQUIRED: generateClientException('LOGOUT_REQUIRED'),
-  FORBIDDEN: generateClientException('FORBIDDEN'),
-  INVALID_TOKEN_FORMAT: generateClientException('INVALID_TOKEN_FORMAT'),
-};
-
-export const UserClientCode = {
-  NOT_FOUND_USER: generateClientException({
-    errorCode: 'USER__NOT_FOUND',
-    message: 'No user(s) found',
-  }),
-  DUPLICATED_USERNAME: generateClientException('USER__DUPLICATED_USERNAME'),
-};
+import { generateSystemException } from './exception-generator';
 
 export const SystemExceptionClientCode = {
   MAINTENANCE: generateSystemException('MAINTENANCE'),
