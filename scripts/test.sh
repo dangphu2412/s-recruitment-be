0ac7b5e2be9b636ac8a4e0127113c1ab34c902e3
Dang Ngoc Phu <48615465+dangphu2412@users.noreply.github.com>
2025-01-04 17:12:06 +0700

    feat: study k8s locally (#114)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo Hello
+AUTH_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMWExOWY0Zi00NGUwLTRjMzgtOWQ1MC1kZTNhM2VmODJiZjciLCJpYXQiOjE3MzU5ODI3MTgsImV4cCI6MTczNTk4MzYxOH0.1NIbdaa3SS9iHa9k_FQ3fdCdr19CD7o86v3UcUdRies"
+BODY='{"paidAt": "2024/10/02","amount": 100000,"note": "test","monthlyConfigId": 1}'
+USER_ID="d7142b4d-ee2a-48e7-8daa-80ab204cbc00"
+seq 1 10 | xargs -n1 -P4 curl -X POST -H "Content-Type: application/json" \
+-H "Authorization: Bearer $AUTH_TOKEN" \
+-d "$BODY" \
+"http://localhost:3001/users/$USER_ID/payments"
