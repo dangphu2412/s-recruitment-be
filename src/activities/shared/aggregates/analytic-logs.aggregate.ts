8879fed2bafda8c497b9edbe95367e0020ab4a1f
dangphu2412 <dangphu241299@gmail.com>
2025-06-29 16:15:42 +0700

    feat: build analytic for bar chart

--- src/activities/shared/aggregates/analytic-logs.aggregate.ts
+++ src/activities/shared/aggregates/analytic-logs.aggregate.ts
@@ -1,10 +1,14 @@
+import { GroupType } from '../../work-logs/dtos/presentation/find-analytic-log.request';
+
 export type AnalyticLogsAggregate = {
   lateCount: number;
   onTimeCount: number;
   notFinishedCount: number;
+  logDate: string;
 };
 
-export type AnalyticLogsAggregateDTO = {
+export type AnalyticLogsAggregateQuery = {
   fromDate: string;
   toDate: string;
+  groupType?: GroupType;
 };
--- src/activities/work-logs/activities-log.controller.ts
+++ src/activities/work-logs/activities-log.controller.ts
@@ -3,7 +3,10 @@ import { ActivityLogService } from './activity-log.service';
 import { FindLogsRequest } from './dtos/presentation/find-logs.request';
 import { CanAccessBy } from '../../account-service/authorization/can-access-by.decorator';
 import { Permissions } from '../../account-service/authorization/access-definition.constant';
-import { FindAnalyticLogRequest } from './dtos/presentation/find-analytic-log.request';
+import {
+  FindAnalyticLogRequest,
+  FindV2AnalyticLogRequest,
+} from './dtos/presentation/find-analytic-log.request';
 
 @Controller('activity-logs')
 export class ActivitiesLogController {
@@ -18,6 +21,13 @@ export class ActivitiesLogController {
   @Get('analytics')
   findAnalyticLogs(@Query() findAnalyticLogRequest: FindAnalyticLogRequest) {
     return this.activityLogService.findAnalyticLogs(findAnalyticLogRequest);
+  }
+
+  @Get('/v2/analytics')
+  findV2AnalyticLogs(
+    @Query() findAnalyticLogRequest: FindV2AnalyticLogRequest,
+  ) {
+    return this.activityLogService.findV2AnalyticLogs(findAnalyticLogRequest);
   }
 
   @CanAccessBy(Permissions.WRITE_ACTIVITY_LOGS)
