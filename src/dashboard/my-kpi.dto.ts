c11ebc43528e9f8e64a3cece5919d02079bcc4f4
dangphu2412 <dangphu241299@gmail.com>
2025-07-01 23:16:46 +0700

    feat: my kpi

---
+++ src/dashboard/my-kpi.dto.ts
@@ -0,0 +1,9 @@
+export type MyKPI = {
+  estimatedPaid: number;
+  totalPayment: number;
+  totalPendingRequests: number;
+  totalLateActivities: number;
+  totalFinishedWork: number;
+  totalToBeFinishedWork: number;
+  remainingToBeFinishedWork: number;
+};
