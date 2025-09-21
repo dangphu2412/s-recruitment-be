b28ad3af43b8155252e8d029a9f1a3907085253b
Dang Ngoc Phu (Justin) <48615465+dangphu2412@users.noreply.github.com>
2025-09-21 12:22:47 +0700

    refactor: enhance file interceptor (#241)

--- src/system/file/file.interceptor.ts
+++ src/system/file/file.interceptor.ts
@@ -9,21 +9,43 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import FastifyMulter from 'fastify-multer';
-import { Options, Multer } from 'multer';
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
 
-export function FileInterceptor(
-  fieldName: string,
-  localOptions?: Options,
-): Type<NestInterceptor> {
+/**
+ * Since Fastify Multer does not export File type so this ref to File implement of fastify-multer
+ */
+export type InternalFile = {
+  /** Field name specified in the form */
+  fieldname: string;
+  /** Name of the file on the user's computer */
+  originalname: string;
+  /** Encoding type of the file */
+  encoding: string;
+  /** Mime type of the file */
+  mimetype: string;
+  /** Size of the file in bytes */
+  size?: number;
+  /** The folder to which the file has been saved (DiskStorage) */
+  destination?: string;
+  /** The name of the file within the destination (DiskStorage) */
+  filename?: string;
+  /** Location of the uploaded file (DiskStorage) */
+  path?: string;
+  /** A Buffer of the entire file (MemoryStorage) */
+  buffer?: Buffer;
+  stream?: NodeJS.ReadableStream;
+};
+
+export function FileInterceptor(fieldName: string): Type<NestInterceptor> {
   class MixinInterceptor implements NestInterceptor {
-    protected multer: Multer;
+    protected multer: ReturnType<typeof FastifyMulter>;
 
     constructor(
       @Optional()
       @Inject('MULTER_MODULE_OPTIONS')
-      options: Multer,
+      options: ReturnType<typeof FastifyMulter>,
     ) {
-      this.multer = (FastifyMulter as any)({ ...options, ...localOptions });
+      this.multer = FastifyMulter({ ...options });
     }
 
     async intercept(
@@ -32,19 +54,23 @@ export function FileInterceptor(
     ): Promise<Observable<any>> {
       const ctx = context.switchToHttp();
 
-      await new Promise<void>((resolve, reject) =>
-        this.multer.single(fieldName)(
-          ctx.getRequest(),
-          ctx.getResponse(),
-          (error: any) => {
-            if (error) {
-              return reject(error);
-            }
+      await new Promise<void>((resolve, reject) => {
+        const handler = this.multer.single(fieldName).bind(this.multer);
 
-            return resolve();
-          },
-        ),
-      );
+        const errorHandler: HookHandlerDoneFunction = (error: Error) => {
+          if (error) {
+            reject(error);
+          }
+
+          resolve();
+        };
+
+        handler(
+          ctx.getRequest<FastifyRequest>(),
+          ctx.getResponse<FastifyReply>(),
+          errorHandler,
+        );
+      });
 
       return next.handle();
     }
